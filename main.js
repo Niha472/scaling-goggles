4ec33afd0701dbcfe0623d1a440d2ae06ff5139a
Niha472 <72551894+Niha472@users.noreply.github.com>
2022-01-07 15:18:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function setup(){
+canvas=createCanvas(500,500)
+canvas.center()
+video=createCapture(VIDEO)
+}
+function preload(){}
+function draw(){}
+function snapshot(){
+save('Me_clown.png')
+}
